dd81dceabdefcb7c89c52f1bf291d215e515cd1f
shahinur <mshahin364@gmail.com>
2020-03-28 20:52:16 +0600

    Implement FlatList

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,21 +1,43 @@
-import React, {Fragment} from 'react';
-import {SafeAreaView, View, Text, ScrollView} from 'react-native';
+import React, {Fragment, useState} from 'react';
+import {SafeAreaView, View, Text, ScrollView, FlatList} from 'react-native';
 
 import nachos from '../data/nachos';
 
 import ListItem, {Separator} from '../components/ListItem';
 
 export default () => {
+  // const [favorite, setFavorite] = useState(false);
   return (
     <SafeAreaView>
-      <ScrollView>
-        {nachos.map(item => (
-          <Fragment key={item.id}>
-            <ListItem name={item.name} />
-            <Separator />
-          </Fragment>
-        ))}
-      </ScrollView>
+      <FlatList
+        data={nachos}
+        renderItem={({item, index}) => (
+          <ListItem
+            name={item.name}
+            onFavoritePress={() => alert('todo')}
+            isFavorite={index < 2}
+          />
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <Separator />}
+      />
     </SafeAreaView>
   );
+
+  // return (
+  //   <SafeAreaView>
+  //     <ScrollView>
+  //       {nachos.map((item, index) => (
+  //         <Fragment key={item.id}>
+  //           <ListItem
+  //             name={item.name}
+  //             onFavoritePress={onFavoritePress}
+  //             isFavorite={index < 2}
+  //           />
+  //           <Separator />
+  //         </Fragment>
+  //       ))}
+  //     </ScrollView>
+  //   </SafeAreaView>
+  // );
 };
